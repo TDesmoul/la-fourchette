b3b9eb4688b32162026028492b5dda9987849b87
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2019-02-18 09:53:06 +0100

    devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/secured_page', to: 'pages#secured_page', as: 'secured'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
